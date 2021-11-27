1904c5deaba2a9b6682066b0c8109b74a96dd668
Johannes Tegnér <johannes@jitesoft.com>
2021-11-27 11:53:52 +0100

    Export variables so envsubst can use them.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ if [ -n "${REVIVE_DOMAIN}" ]; then
   echo "REVIVE_DOMAIN variable set. Checking for configuration"
   if [ ! -f "/var/www/html/var/${REVIVE_DOMAIN}.conf.php" ]; then
     echo "Configuration not found, generating dynamic configuration"
-    REVIVE_DB_PREFIX="${REVIVE_DB_PREFIX:-rv_}"
-    REVIVE_DB_TYPE="${REVIVE_DB_TYPE:-mysqli}"
+    export REVIVE_DB_PREFIX=${REVIVE_DB_PREFIX:-rv_}
+    export REVIVE_DB_TYPE=${REVIVE_DB_TYPE:-mysqli}
     TMP_RAND_STRING=$(LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom | head -c 32 | base64)
-    REVIVE_DELIVERY_SECRET="${REVIVE_DELIVERY_SECRET:-$TMP_RAND_STRING}"
-    REVIVE_APPLICATION_NAME="${REVIVE_APPLICATION_NAME:-Revive}"
-    REVIVE_ASYNC_JS_SCRIPT="${REVIVE_ASYNC_JS_SCRIPT:-new-async}"
+    export REVIVE_DELIVERY_SECRET=${REVIVE_DELIVERY_SECRET:-$TMP_RAND_STRING}
+    export REVIVE_APPLICATION_NAME=${REVIVE_APPLICATION_NAME:-Revive}
+    export REVIVE_ASYNC_JS_SCRIPT=${REVIVE_ASYNC_JS_SCRIPT:-new-async}
     envsubst < /usr/local/conf.tmpl > "/var/www/html/var/${REVIVE_DOMAIN}.conf.php"
     echo "Created new config and stored it in /var/www/html/var/${REVIVE_DOMAIN}.conf.php"
   else
